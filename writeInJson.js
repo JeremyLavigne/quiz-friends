e89c091039012518bd1cc7a39683d075690c1ba7
Jeremy <lavigne.jeremy@post.com>
2021-02-25 16:20:30 +0100

    Template for writing Json file based on text data

---
+++ writeInJson.js
@@ -0,0 +1,29 @@
+// const fs = require('fs');
+// const readline = require('readline');
+
+// var rd = readline.createInterface({
+//     input: fs.createReadStream('./temp'),
+//     // output: process.stdout,
+//     console: false
+// });
+
+// rd.on('line', function(line) {
+//     console.log(line);
+// });
+
+// rd.on('close', function() {
+//     console.log('done')
+// });
+
+// const data = [];
+
+// for (let i = 0; i < length; i += 1) {
+//     data.push({});
+// }
+
+// const toWrite = JSON.stringify(data, null, 2);
+
+// fs.writeFile('./path.json', toWrite, function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
