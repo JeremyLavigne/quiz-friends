fe00f2619e0b402c0d50f36f57c40c8ddec49d39
Jeremy Lavigne <lavigne.jeremy@post.com>
2021-03-01 17:43:57 +0100

    Add some style in cards.

--- components/CardOnPile.js
+++ components/CardOnPile.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+import backFriends from "../assets/friends.jpg";
 
 export default function App({ setCardIsOnReading, cardIsDrawn, unread, card }) {
   return (
@@ -12,11 +13,12 @@ export default function App({ setCardIsOnReading, cardIsDrawn, unread, card }) {
             cardIsDrawn(card);
           }}
         >
-          <Text>{card.id}</Text>
+          <Image source={backFriends} style={styles.image}></Image>
         </TouchableOpacity>
       ) : (
         <View style={card !== null && styles.cardRead}>
-          <Text>{card !== null && card.id}</Text>
+          <Text style={styles.question}>{card && card.question}</Text>
+          <Text style={styles.answer}>{card && card.answer}</Text>
         </View>
       )}
     </>
@@ -27,7 +29,6 @@ const styles = StyleSheet.create({
   cardUnread: {
     width: "60%",
     height: "30%",
-    backgroundColor: "blue",
     borderRadius: 10,
     marginBottom: 30,
     elevation: 5,
@@ -35,8 +36,28 @@ const styles = StyleSheet.create({
   cardRead: {
     width: "60%",
     height: "30%",
-    backgroundColor: "green",
+    backgroundColor: "white",
+    borderWidth: 5,
+    borderColor: "black",
     borderRadius: 10,
     marginTop: 30,
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+  },
+  question: {
+    margin: 10,
+    marginTop: 20,
+    textAlign: "justify",
+    fontSize: 18,
+  },
+  answer: {
+    margin: 10,
+    fontSize: 14,
+    textAlign: "right",
   },
 });
--- components/CardOnReading.js
+++ components/CardOnReading.js
@@ -1,18 +1,35 @@
-import React from "react";
-import { Button, Text, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, StyleSheet, View, TouchableOpacity } from "react-native";
 
 export default function App({ setCardIsOnReading, cardIsDiscarded, card }) {
+  const [displayAnswer, setDisplayAnswer] = useState(false);
+
   return (
     <View style={styles.cardOnReading}>
-      <Text>{card.question}</Text>
-      <Text>{card.answer}</Text>
-      <Button
-        onPress={() => {
-          setCardIsOnReading(false);
-          cardIsDiscarded(card);
-        }}
-        title="Close"
-      />
+      <Text style={styles.question}>{card && card.question}</Text>
+
+      {displayAnswer ? (
+        <View style={styles.answerContainer}>
+          <Text style={styles.answer}>{card && card.answer}</Text>
+        </View>
+      ) : (
+        <TouchableOpacity
+          style={styles.revealContainer}
+          onPress={() => setDisplayAnswer(true)}
+        >
+          <Text style={styles.reveal}>Reveal Answer</Text>
+        </TouchableOpacity>
+      )}
+      <View style={styles.closeButton}>
+        <Button
+          onPress={() => {
+            setCardIsOnReading(false);
+            cardIsDiscarded(card);
+          }}
+          title="Close"
+          color="red"
+        />
+      </View>
     </View>
   );
 }
@@ -21,8 +38,46 @@ const styles = StyleSheet.create({
   cardOnReading: {
     width: "80%",
     height: "80%",
-    backgroundColor: "red",
+    backgroundColor: "white",
+    borderWidth: 5,
+    borderColor: "black",
     borderRadius: 10,
     marginTop: 30,
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  question: {
+    margin: 10,
+    marginTop: 30,
+    textAlign: "justify",
+    fontSize: 22,
+  },
+  answerContainer: {
+    margin: 10,
+    paddingLeft: 5,
+    paddingVertical: 10,
+    borderColor: "black",
+    borderWidth: 2,
+  },
+  revealContainer: {
+    margin: 10,
+    paddingLeft: 5,
+    paddingVertical: 10,
+    backgroundColor: "grey",
+  },
+  reveal: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  answer: {
+    color: "black",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  closeButton: {
+    width: "30%",
+    marginLeft: "35%",
+    padding: 5,
   },
 });
