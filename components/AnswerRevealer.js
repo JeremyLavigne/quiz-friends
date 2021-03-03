1bd26628c520ef6dfa386ea79d55e3a747d8ccac
Jeremy Lavigne <lavigne.jeremy@post.com>
2021-03-03 21:22:49 +0100

    Keep working on answer revealer.

--- components/AnswerRevealer.js
+++ components/AnswerRevealer.js
@@ -4,29 +4,57 @@ import { Animated, View, StyleSheet, PanResponder, Text } from "react-native";
 // =================================================================================
 export default function AnswerRevealer({ answer }) {
   const [displayAnswer, setDisplayAnswer] = useState(false);
+  const [openerRightWidth, setOpenerRightWidth] = useState("96%");
 
-  // const pan = useRef(new Animated.ValueXY()).current;
-  // const panResponder = useRef(
-  //   PanResponder.create({
-  //     onMoveShouldSetPanResponder: () => true,
-  //     onPanResponderMove: Animated.event([null, { dx: pan.x }]),
-  //     onPanResponderRelease: () => {
-  //       Animated.spring(pan, { toValue: { x: 0 } }).start();
-  //     },
-  //   })
-  // ).current;
+  // const handleOnPanResponderMove = (Xpos) => {
+  //   const widthTry = 800; // Should be the View total width
+  //   alert("here");
+
+  //   const percent = Math.floor(widthTry - widthTry / Xpos);
+  //   const cssWidth = percent.toString() + "%";
+  //   if (cssWidth !== openerRightWidth) {
+  //     setOpenerRightWidth(cssWidth);
+  //   }
+
+  //   return Animated.event([null, { dx: pan.x, dy: pan.y }], {});
+  // };
+
+  const pan = useRef(new Animated.ValueXY()).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], {}),
+      onPanResponderRelease: () => {
+        Animated.spring(pan, {
+          toValue: { x: 0, y: 0 },
+        }).start();
+      },
+    })
+  ).current;
 
   return (
     <View style={styles.container}>
-      {/* <Animated.View
+      <Animated.View
         style={{
+          zIndex: 3,
           transform: [{ translateX: pan.x }],
         }}
         {...panResponder.panHandlers}
       >
-        <View style={styles.box} />
-      </Animated.View>  */}
+        <View style={styles.opener} />
+      </Animated.View>
+
       <Text style={styles.answer}>{answer}</Text>
+      <View
+        style={{
+          position: "absolute",
+          right: 0,
+          width: openerRightWidth,
+          height: "100%",
+          backgroundColor: "green",
+          zIndex: 1,
+        }}
+      ></View>
     </View>
   );
 }
@@ -37,52 +65,23 @@ const styles = StyleSheet.create({
     flex: 1,
     width: "96%",
     maxHeight: 100,
-    backgroundColor: "pink",
-    alignItems: "center",
+    backgroundColor: "grey",
     justifyContent: "center",
     marginHorizontal: "2%",
+    borderRadius: 5,
   },
-  titleText: {
-    fontSize: 14,
-    lineHeight: 24,
-    fontWeight: "bold",
-  },
-  box: {
-    height: 100,
-    width: 30,
+  opener: {
+    height: 106,
+    width: 40,
     backgroundColor: "blue",
     borderRadius: 5,
   },
-  answerContainer: {
-    margin: 10,
-    paddingLeft: 5,
-    paddingVertical: 10,
-    borderColor: "black",
-    borderWidth: 2,
-  },
-  revealContainer: {
-    margin: 10,
-    paddingLeft: 5,
-    paddingVertical: 10,
-    backgroundColor: "grey",
-  },
-  reveal: {
+  answer: {
+    position: "absolute",
+    left: 10,
     color: "white",
     textAlign: "center",
     fontSize: 18,
-  },
-  answer: {
-    color: "black",
-    textAlign: "center",
-    fontSize: 18,
+    zIndex: 0,
   },
 });
-
-/*
-        <TouchableOpacity
-          style={styles.revealContainer}
-          onPress={() => setDisplayAnswer(true)}
-        >
-          <Text style={styles.reveal}>Reveal Answer</Text>
-        </TouchableOpacity>
-*/
